8d7a96c48af5064f7a81e520f142070e1ba8daac
maksimkolesnikov <58657900+maksimkolesnikov@users.noreply.github.com>
2020-06-27 20:05:05 +0300

    Lesson3_result
    Upload lesson3

---
+++ Lesson3/js/script_multiplication.js
@@ -0,0 +1,8 @@
+let a = prompt('Enter a');
+let b = prompt('Enter b');
+function multiplication(a,b) {
+    const result = a * b;
+    return result; 
+}
+const result = multiplication(a,b);
+console.log(result);
